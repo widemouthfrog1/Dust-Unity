ef1e0dc6e4188c5631658ce264664351b495ad9a
Karl Bennett <widemouthfrog1@gmail.com>
2020-02-08 16:15:57 +1300

    Initial Commit

---
+++ Assets/Scripts/RuneParts/Abstract_Part.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Abstract_Part
+{
+    protected List<Vector2> points;
+
+    public Abstract_Part(List<Vector2> points)
+    {
+        this.points = points;
+    }
+
+    protected List<Vector2> Abstract_Sample(int sparseness)
+    {
+        List<Vector2> newPoints = new List<Vector2>();
+        for(int i = 0; i < points.Count; i++)
+        {
+            if(i % sparseness == 0)
+            {
+                newPoints.Add(points[i]);
+            }
+        }
+        return newPoints;
+    }
+    protected List<Vector2> Abstract_RemoveDuplicates()
+    {
+        List<Vector2> newPoints = new List<Vector2>();
+        for (int i = 0; i < points.Count; i++)
+        {
+            if (i < 1 || !points[i].Equals(points[i-1]))
+            {
+                newPoints.Add(points[i]);
+            }
+        }
+        return newPoints;
+    }
+
+    protected bool Abstract_Line(float tolerance)
+    {
+        Vector2 start = points[0];
+        Vector2 end = points[points.Count - 1];
+        Vector2 line = end - start;
+        for (int i = 1; i < points.Count - 1; i++)
+        {
+            Vector2 startToPoint = points[i] - start;
+            float angle = Vector2.Angle(line, startToPoint);
+            float distanceFromLine = startToPoint.magnitude * Mathf.Sin(angle);
+            if (distanceFromLine > tolerance)
+            {
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+    protected List<Vector2> Get_Corners(float degreesAngleTolerance)
+    {
+        if(points.Count < 2)
+        {
+            return null;
+        }
+        Vector2 start = points[0];
+        Vector2 end = points[points.Count - 1];
+        List<Vector2> corners = new List<Vector2>();
+        
+        corners.Add(start);
+        if (points.Count < 3)
+        {
+            corners.Add(end);
+            return corners;
+        }
+        for (int i = 2; i < points.Count; i++)
+        {
+            Vector2 currentLine = points[i - 1] - start;
+            Vector2 nextLine = points[i] - points[i - 1];
+            if(Vector2.Angle(currentLine, nextLine) < 180f - degreesAngleTolerance)
+            {
+                corners.Add(points[i - 1]);
+                start = points[i - 1];
+            }
+        }
+
+        corners.Add(end);
+        return corners;
+    }
+}
---
+++ Assets/Scripts/RuneParts/Part.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Part : Abstract_Part
+{
+    public Part(List<Vector2> points) : base(points){}
+
+    public Part RemoveDuplicates()
+    {
+        return new Part(Abstract_RemoveDuplicates());
+    }
+
+    public Part Sample()
+    {
+        return new Part(Abstract_Sample(5));
+    }
+
+    public Part Sample(int sparseness)
+    {
+        return new Part(Abstract_Sample(sparseness));
+    }
+
+    public Part Default_Preprocess()
+    {
+        return RemoveDuplicates().Sample();
+    }
+
+    public Part[] SplitByCorners()
+    {
+        List<Vector2> corners = Get_Corners(10);
+        Part[] parts = new Part[corners.Count - 1];
+        int corner = 1;
+        List<Vector2> newPoints = new List<Vector2>();
+        for (int i = 0; i < points.Count; i++)
+        {
+            newPoints.Add(points[i]);
+            if (points[i].Equals(corners[corner]))
+            {
+                parts[corner - 1] = new Part(newPoints);
+                newPoints = new List<Vector2>();
+                i--; //to add this point (the corner) to the next part
+                corner++;
+            }
+        }
+        return parts;
+    }
+
+    public Part[] SplitByCorners(float degreesAngleTolerance)
+    {
+        List<Vector2> corners = Get_Corners(degreesAngleTolerance);
+        Part[] parts = new Part[corners.Count - 1];
+        int corner = 1;
+        List<Vector2> newPoints = new List<Vector2>();
+        for (int i = 0; i < points.Count; i++)
+        {
+            newPoints.Add(points[i]);
+            if (points[i].Equals(corners[corner]))
+            {
+                parts[corner - 1] = new Part(newPoints);
+                newPoints = new List<Vector2>();
+                i--; //to add this point (the corner) to the next part
+                corner++;
+            }
+        }
+        return parts;
+    }
+
+    public bool Line()
+    {
+        return Abstract_Line(0.5f);
+    }
+
+    public bool Line(float tolerance)
+    {
+        return Abstract_Line(tolerance);
+    }
+
+    public bool Arrow()
+    {
+        List<Vector2> corners = Get_Corners(10);
+        if(corners.Count != 3)
+        {
+            return false;
+        }
+        Part[] parts = SplitByCorners();
+        return parts[0].Line() && parts[1].Line();
+    }
+
+    public bool Arrow(float degreesAngleTolerance, float lineTolerance)
+    {
+        List<Vector2> corners = Get_Corners(10);
+        if (corners.Count != 3)
+        {
+            return false;
+        }
+        Part[] parts = SplitByCorners(degreesAngleTolerance);
+        return parts[0].Line(lineTolerance) && parts[1].Line(lineTolerance);
+    }
+
+}
